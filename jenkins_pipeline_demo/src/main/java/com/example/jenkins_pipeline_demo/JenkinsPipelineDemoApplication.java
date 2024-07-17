88d414e3c50575f450799a0b40f524910bcff770
bphad4299 <bphad.miscellaneous@gmail.com>
2024-07-17 14:08:08 +0530

    initial commit

---
+++ jenkins_pipeline_demo/src/main/java/com/example/jenkins_pipeline_demo/JenkinsPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_pipeline_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineDemoApplication.class, args);
+	}
+
+}
---
+++ jenkins_pipeline_demo/src/test/java/com/example/jenkins_pipeline_demo/JenkinsPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_pipeline_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
