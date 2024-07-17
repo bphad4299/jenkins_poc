e115f841deb06e6ca0040c18bfa8bfc1cbf92c90
bphad4299 <bphad.miscellaneous@gmail.com>
2024-07-17 14:34:40 +0530

    updated

--- jenkins_pipeline_demo/src/main/java/com/example/jenkins_pipeline_demo/JenkinsPipelineDemoApplication.java
+++ jenkins_pipeline_demo/src/main/java/com/example/jenkins_pipeline_demo/JenkinsPipelineDemoApplication.java
@@ -2,12 +2,19 @@ package com.example.jenkins_pipeline_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsPipelineDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPipelineDemoApplication.class, args);
 	}
 
+	@RequestMapping(value = "/name")
+	public String getMessage() {
+		return "Jenkins demo";
+	}
 }
